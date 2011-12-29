82feeb29a374c41ecc4d90377738e900ff375522
Gelgamek <gelgamek@arcor.de>
2011-12-29 17:13:24 +0100

    Scroll-Handler impl.

--- hommk_shortcuts.user.js
+++ hommk_shortcuts.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name          HkToolkit
-// @version       2011.12.29.16.58.330000
+// @version       2011.12.29.17.03.040000
 // @description   Werkzeugkasten für HOMMK
 // @author        Gelgamek <gelgamek@arcor.de>
 // @copyright	  Gelgamek et al., Artistic License 2.0, http://www.opensource.org/licenses/Artistic-2.0
@@ -78,7 +78,7 @@ w.hkCreateClasses = function () {
   window.Hk = new Class({
 	$debug: 1,
 	idScript: "HkToolkit",
-	version: "2011.12.29.16.58.330000",
+	version: "2011.12.29.17.03.040000",
 	Coords: {
 	  lastRegion: {
 		x: 0,
@@ -422,6 +422,11 @@ w.hkCreateClasses = function () {
 	  var evtRt = $(winId).getElement(".HkContent");
 	  window.hk.log(evtRt);
 	  window.hk.log(evtRt.getSize());
+	  var size = evtRt.getSize().size;
+	  var scroll = evtRt.getSize().scroll;
+	  var scrollSize = evtRt.getSize().scrollSize;
+	  var scrollToY = size.y - 20 < scroll.y ? scoll.y : size.y - 20;
+	  evtRt.scrollTo(scroll.x, scrollToY);
 	  window.hk.log(evtRt.getPosition());
 	  window.hk.log(evtRt.getCoordinates());
 	  window.hk.log(evtRt.getPosition({
@@ -434,6 +439,24 @@ w.hkCreateClasses = function () {
 	scrollDown: function scrollDown(evt) {
 	  window.hk.log('[HkWindow][DEBUG]scrollDown:');
 	  window.hk.log(evt);
+	  window.hk.log(evt.target);
+	  var winId = evt.target.btnWindow.getWindowId(evt.target.srcId, evt.target.btnOpts);
+	  window.hk.log('[HkWindow][DEBUG]Fenster-ID: ' + winId);
+	  var evtRt = $(winId).getElement(".HkContent");
+	  window.hk.log(evtRt);
+	  window.hk.log(evtRt.getSize());
+	  var size = evtRt.getSize().size;
+	  var scrollSize = evtRt.getSize().scrollSize;
+	  var scrollToY = size.y + 20 > scrollSize.y ? scollSize.y : size.y + 20;
+	  evtRt.scrollTo(scroll.x, scrollToY);
+	  window.hk.log(evtRt.getPosition());
+	  window.hk.log(evtRt.getCoordinates());
+	  window.hk.log(evtRt.getPosition({
+		'overflown': [$(winId)]
+	  }));
+	  window.hk.log(evtRt.getCoordinates({
+		'overflown': [$(winId)]
+	  }));
 	},
 	getId: function getId(base, id, options) {
 	  return base + $pick($pick(id, this.options.id), this.id);
