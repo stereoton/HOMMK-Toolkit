30f9f85d9d950e6829af128819b7eb4f62b947fe
Gelgamek <gelgamek@arcor.de>
2012-01-06 12:54:59 +0100

    Dbg DragComplete Event-Objekt

--- hommk_shortcuts.user.js
+++ hommk_shortcuts.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name          HkToolkit
-// @version       2012.01.06.12.30.370000
+// @version       2012.01.06.12.32.580000
 // @description   Werkzeugkasten für HOMMK
 // @author        Gelgamek <gelgamek@arcor.de>
 // @copyright	  Gelgamek et al., Artistic License 2.0, http://www.opensource.org/licenses/Artistic-2.0
@@ -81,7 +81,7 @@ w.hkCreateClasses = function () {
   window.Hk = new Class({
 	$debug: 1,
 	idScript: "HkToolkit",
-	version: "2012.01.06.12.30.370000",
+	version: "2012.01.06.12.32.580000",
 	Coords: {
 	  lastRegion: {
 		x: 0,
@@ -553,6 +553,7 @@ w.hkCreateClasses = function () {
 		this.windows.push(windowNode);
 	  }
 	  windowNode.hkWindow = this;
+	  windowNode.setProperty("hkWindow", this);
 	  return windowNode;
 	},
 	getWindowPosition: function getWindowPosition(id, options) {
