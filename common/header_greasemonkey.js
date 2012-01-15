97aebe263c50f74f47de3fb9701acf7ec46a07f4
Gelgamek <gelgamek@arcor.de>
2012-01-15 11:15:22 +0100

    Template für GM-Header
    Signed-off-by: Gelgamek <gelgamek@arcor.de>

---
+++ common/header_greasemonkey.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name $Name$
+// $Version$
+// @description $Description$
+// @author Gelgamek <gelgamek@arcor.de>
+// @copyright Gelgamek et al., Artistic License 2.0, http://www.opensource.org/licenses/Artistic-2.0
+// @icon http://icons.iconarchive.com/icons/webiconset/mobile/32/maps-icon.png
+// @namespace http://mightandmagicheroeskingdoms.ubi.com/play
+// @match http://mightandmagicheroeskingdoms.ubi.com/play*
+//
+// Content Scope Runner:
+// @require http://userscripts.org/scripts/source/68059.user.js
+// ==/UserScript==
+
