94c9c245d4699f098b5b5ed314778dc6ddda6cc0
Gelgamek <gelgamek@arcor.de>
2012-01-20 10:41:07 +0100

    Fix: $LoaderConditions$
    Signed-off-by: Gelgamek <gelgamek@arcor.de>

--- common/hommk_loader.js
+++ common/hommk_loader.js
@@ -1,5 +1,5 @@
 
-//window.console.log('Erzeuge Loader-Objekt\u2026');
+window.console.log('[$Name$][PUBLIC]Erzeuge $Name$-Loader-Objekt\u2026');
 
 /**
  * Verfügbarkeit der HOMMK-Objekte prüfen
@@ -18,7 +18,7 @@ if(!window.hasOwnProperty("$Name$Loader")) {
 			    }
 			    return;
 		    }
-		    //window.console.log('[$Name$Loader][DEBUG]Warte auf Objekte\u2026');
+		    window.console.log('[$Name$Loader][DEBUG]Warte auf Objekte\u2026');
 		    if(!!($LoaderConditions$ && window.$Name$CreateClasses && window.$Name$DependentObjectsAvailable && window.$Name$DependentObjectsAvailable())) {
 			    window.console.log('[$Name$Loader][DEBUG]Objekte verfügbar, bereite $Name$ vor\u2026');
 			    try {
@@ -29,14 +29,14 @@ if(!window.hasOwnProperty("$Name$Loader")) {
 				    window.console.log('[$Name$Loader][ERROR]Fehler beim Erzeugen der Klassen für $Name$: ' + ex);
 			    }
 		    } else {
-			    //window.console.log('[$Name$Loader][DEBUG]$Name$ wartet auf die Verfügbarkeit von Objekten.');
+			    window.console.log('[$Name$Loader][DEBUG]$Name$ wartet auf die Verfügbarkeit von Objekten.');
 		    }
 	    }
 	};
 	
 
 }
-//window.console.log('Starte Loader\u2026');
+window.console.log('[$Name$][PUBLIC]Starte $Name$-Loader\u2026');
 
 /**
  * Alle 1000ms die Verfügbarkeit prüfen.
@@ -46,5 +46,5 @@ try {
 		window.$Name$LoaderActive = window.$Name$Loader.load.periodical(5000);
 	}
 } catch(ex) {
-	window.console.log('[HkPublic][DEBUG]Fehler beim Laden von $Name$: ' + ex);
+	window.console.log('[$Name$][DEBUG]Fehler beim Laden von $Name$: ' + ex);
 }
