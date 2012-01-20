7dcc7cdef2b2b3e8863e00a3bf4ceb5b79ad15e7
Gelgamek <gelgamek@arcor.de>
2012-01-20 11:39:35 +0100

    Konsolenausgabe in try-catch
    Signed-off-by: Gelgamek <gelgamek@arcor.de>

--- common/hommk_logger.js
+++ common/hommk_logger.js
@@ -4,12 +4,18 @@ if('undefined' == typeof window.console) {
 			return msg;
 		}
 	};
-} else {
+} else if('undefined' == typeof window.console.logNative) {
 	window.console.logNative = window.console.log;
 	window.console.log = function(msg, debug) {
 		if('function' == typeof window.console.logNative) {
 			var dbg = ("undefined" != typeof window.$debug ? window.$debug : (("undefined" != typeof debug) ? debug : 0));
-			if(dbg >= 1) window.console.logNative(msg);
+			if(dbg >= 1) {
+				try {
+					window.console.logNative(msg);
+				} catch(ex) {
+
+				}
+			}
 			if(dbg > 1) alert(msg);
 		}
 		return msg;
@@ -19,7 +25,11 @@ if('undefined' == typeof window.console) {
 if('undefined' == typeof window.HkLogger) {
 	var HkLogger = new Class({
 		log: function log(msg) {
-			window.console.log(msg);
+			try {
+				window.console.log(msg);
+			} catch(ex) {
+
+			}
 		}
 	});
 	window.HkLogger = HkLogger;
