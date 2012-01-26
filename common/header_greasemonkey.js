41191fb2ad4affa21550b5512095b0cf8c045cf1
Gelgamek <gelgamek@arcor.de>
2012-01-26 01:35:36 +0100

    Dbg
    Signed-off-by: Gelgamek <gelgamek@arcor.de>

--- common/header_greasemonkey.js
+++ common/header_greasemonkey.js
@@ -4,7 +4,7 @@
 // @description $Description$
 // @author Gelgamek <gelgamek@arcor.de>
 // @copyright Gelgamek et al., Artistic License 2.0, http://www.opensource.org/licenses/Artistic-2.0
-// @icon $Icon$
+// @icon http://icons.iconarchive.com/icons/webiconset/mobile/32/maps-icon.png
 // @namespace http://mightandmagicheroeskingdoms.ubi.com/play
 // @match http://mightandmagicheroeskingdoms.ubi.com/play*
 //
