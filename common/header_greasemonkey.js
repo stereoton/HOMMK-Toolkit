a7d6451658817ff317a04109952579ea57066e3f
Gelgamek <gelgamek@arcor.de>
2012-01-26 12:07:28 +0100

    +GM Auto-Update
    Signed-off-by: Gelgamek <gelgamek@arcor.de>

--- common/header_greasemonkey.js
+++ common/header_greasemonkey.js
@@ -1,16 +1,20 @@
 // ==UserScript==
 // @name $Name$
 // $Version$
+//
 // @description $Description$
+//
 // @author Gelgamek <gelgamek@arcor.de>
 // @copyright Gelgamek et al., Artistic License 2.0, http://www.opensource.org/licenses/Artistic-2.0
-// @icon $Icon$
-// @namespace http://mightandmagicheroeskingdoms.ubi.com/play
-// @match http://mightandmagicheroeskingdoms.ubi.com/play*
+//
+// @updateURL		$updateURL$
+// @installURL  $installURL$
+//
+// @icon 				$Icon$
+// @namespace 	http://mightandmagicheroeskingdoms.ubi.com/play
+// @match 			http://mightandmagicheroeskingdoms.ubi.com/play*
 //
 // Content Scope Runner:
-// @require http://userscripts.org/scripts/source/68059.user.js
-// @updateURL      $updateURL$
-// @installURL     $installURL$
+// @require 		http://userscripts.org/scripts/source/68059.user.js
 // ==/UserScript==
 
